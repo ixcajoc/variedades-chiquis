32b101f6b0138a78c58b73b623325d3c8f2dfcef
Denis <denisixcajoc2001@gmail.com>
2024-10-11 17:37:16 -0600

    Complete el component completar pedido, agregamos un stepper con tres pasos

--- src/app/components/completar-pedido/completar-pedido.component.ts
+++ src/app/components/completar-pedido/completar-pedido.component.ts
@@ -1,12 +1,94 @@
-import { Component } from '@angular/core';
+import {Component, inject, AfterViewInit, ViewChild,ChangeDetectionStrategy} from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 
 @Component({
   selector: 'app-completar-pedido',
   standalone: true,
-  imports: [],
+  imports: [
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatDatepickerModule,
+  ],
   templateUrl: './completar-pedido.component.html',
-  styleUrl: './completar-pedido.component.css'
+  styleUrl: './completar-pedido.component.css',
+
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {displayDefaultIndicatorType: false},
+    },
+    provideNativeDateAdapter()
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class CompletarPedidoComponent {
+
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    nombre: ['', Validators.required],
+    apellido: ['', Validators.required],
+    correo: ['', Validators.required],
+    telefono: ['', Validators.required],
+    direccion: ['', Validators.required],
+    municipio: ['', Validators.required],
+    departamento: ['', Validators.required],
+  });
+  
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
 
 }

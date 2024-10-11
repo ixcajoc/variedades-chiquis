720a4fd0178a92bf9a033ef953ab875280b3360c
Denis <denisixcajoc2001@gmail.com>
2024-10-11 17:40:59 -0600

    Agregue la nueva linea de tiempo para el detalle del rastreo

--- src/app/components/detalle-rastreo/detalle-rastreo.component.ts
+++ src/app/components/detalle-rastreo/detalle-rastreo.component.ts
@@ -1,17 +1,47 @@
-
 import {Component, inject} from '@angular/core';
 
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+
+
+interface EventItem {
+  status?: string;
+  date?: string;
+  icon?: string;
+  color?: string;
+  image?: string;
+}
 
 @Component({
   selector: 'app-detalle-rastreo',
   standalone: true,
-  imports: [ 
+  imports: [
+    TimelineModule, 
+    CardModule, 
+    ButtonModule,
+    CommonModule 
   ],
   templateUrl: './detalle-rastreo.component.html',
+  styles: `
+    
+  `
+  
   
 })
 
 export default class DetalleRastreoComponent {
+  events: EventItem[];
+
+  constructor() {
+    this.events = [
+        { status: 'Ordenado', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
+        { status: 'En proceso', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+        { status: 'Enviado', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800' },
+        { status: 'Entregado', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B' }
+    ];
+}
   
 
 
